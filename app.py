5a33b10e8fec31878bdd3bc3eb845c995b2a3b57
Venkatesh-Bollarapu <venkateshbollarapu123@gmail.com>
2026-04-22 00:30:38 +0545

    Initial Streamlit chatbot UI setup

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.title("🤖 Chat App")
+
+user_input = st.chat_input("Ask anything")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+
+for msg in st.session_state["messages"]:
+    with st.chat_message(msg["role"]):
+        st.write(msg["content"])
+
+if user_input:
+    st.session_state["messages"].append({
+        "role": "user",
+        "content": user_input
+    })
+
+    with st.chat_message("user"):
+        st.write(user_input)
